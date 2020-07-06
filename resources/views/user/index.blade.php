94cae94ec1abf219d6e6178693a62606589a0132
modex9 <modestas.slivinskas@gmail.com>
2020-07-07 00:55:50 +0300

    removed unnecessary js

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,12 +1,7 @@
 @extends('layouts.app')
-<script type="text/javascript">
-    const banUrl = "{{route('ban.execute')}}";
-    const unbanUrl = "{{route('ban.amend')}}";
-</script>
-<script src="{{ asset('js/admin/users.js') }}" defer></script>
 @section('content')
     <div id="app">
-        <table-component users-route="{{route('fetchUsers')}}" roles-route="{{route('fetchRoles')}}" ban-types-route="{{route('fetchBantypes')}}"></table-component>
+        <table-component  ref="foo" users-route="{{route('fetchUsers')}}" roles-route="{{route('fetchRoles')}}" ban-types-route="{{route('fetchBantypes')}}"></table-component>
     </div>
     <script type="application/javascript" src="{{asset('js/app.js')}}"></script>
 @endsection
